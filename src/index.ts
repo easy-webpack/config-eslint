7955f30c9b122fcf61a13520766202bc2d39c962
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-06-23 14:16:07 +0200

    feat(index): use main module export

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as path from 'path'
  * Tslint loader support for *.ts files
  * See: https://github.com/wbuchwalter/tslint-loader
  */
-export function eslint(options: any = {}, exclude: Array<string> = null) {
+export = function eslint(options: any = {}, exclude: Array<string> = null) {
   return function eslint(this: WebpackConfig): WebpackConfig {
     return {
       module: {
